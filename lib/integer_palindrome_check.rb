d97048ec6519f78e0773747bbdf6fe5b4d2f9e93
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-03-29 18:01:17 -0700

    All tests passing, method retains O(1) space complexity

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,56 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  if number == nil
+    return false
+  end
+
+  number1 = number.to_s.chars
+
+  if number1.length == 0
+    return true
+  end
+  i = 0
+  j = number1.length - 1
+
+  while i < j
+    if number1[i] != number1[j]
+      # elsif phrase1[i] != phrase1[-i]
+      return false
+    end
+    i += 1
+    j -= 1
+  end
+  return true
 end
+
+# def palindrome_check(my_phrase)
+#   if my_phrase == nil
+#     return false
+#   end
+#
+#   phrase1 = my_phrase.chars
+#
+#   if phrase1.length == 0
+#     return true
+#   end
+#
+#   i = 0
+#   j = phrase1.length - 1
+#   while i < j
+#     while phrase1[i] == " "
+#       i += 1
+#     end
+#
+#     while phrase1[j] == " "
+#       j -= 1
+#     end
+#
+#     if phrase1[i] != phrase1[j]
+#       # elsif phrase1[i] != phrase1[-i]
+#       return false
+#     end
+#     i += 1
+#     j -= 1
+#   end
+#   return true
+# end
