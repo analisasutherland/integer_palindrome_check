47e69961d94a319e718f67ae35963d20af876f74
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-03-29 18:02:24 -0700

    Removed extrenuous comments.

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -14,7 +14,6 @@ def is_palindrome(number)
 
   while i < j
     if number1[i] != number1[j]
-      # elsif phrase1[i] != phrase1[-i]
       return false
     end
     i += 1
@@ -22,35 +21,3 @@ def is_palindrome(number)
   end
   return true
 end
-
-# def palindrome_check(my_phrase)
-#   if my_phrase == nil
-#     return false
-#   end
-#
-#   phrase1 = my_phrase.chars
-#
-#   if phrase1.length == 0
-#     return true
-#   end
-#
-#   i = 0
-#   j = phrase1.length - 1
-#   while i < j
-#     while phrase1[i] == " "
-#       i += 1
-#     end
-#
-#     while phrase1[j] == " "
-#       j -= 1
-#     end
-#
-#     if phrase1[i] != phrase1[j]
-#       # elsif phrase1[i] != phrase1[-i]
-#       return false
-#     end
-#     i += 1
-#     j -= 1
-#   end
-#   return true
-# end
